a437a76f483b67fa0abbb293f8ecffd465e84aed
Flying Serpent <nickprather11@gmail.com>
2019-08-08 16:29:15 -0400

    apiRoutes and htmlRoutes required in server.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,13 @@
-app.get("/api/friends", function(req, res) {
-  return res.json(friendsArray);
-});
+var friendsArray = require("../data/friends");
+function apiRoutes(app) {
+  app.get("/api/friends", function(req, res) {
+    return res.json(friendsArray);
+  });
 
-app.post("/api/tables", function(req, res) {
-  //convert input from form to object and push said object to friendsArray
-  //COMPATABILITY LOGIC
-});
+  app.post("/api/friends", function(req, res) {
+    //convert input from form to object and push said object to friendsArray
+    //COMPATABILITY LOGIC
+  });
+}
+
+module.exports = apiRoutes;
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require("express"),
   path = require("path"),
   app = express(),
   PORT = 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
